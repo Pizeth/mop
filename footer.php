96aa9b0da44490a93c5f511c898b6b11a128d247
Razeth <seth.razeth@gmail.com>
2020-09-22 10:39:39 +0700

    last

--- footer.php
+++ footer.php
@@ -530,6 +530,25 @@
                 //         'autoWidth'   : false
                 //     });
 	           });
+
+    if ('loading' in HTMLImageElement.prototype) {
+        //Native Lazy Loading Supported!
+
+        console.log('yay!')
+
+        const images = document.querySelectorAll('img[loading="lazy"]')
+        images.forEach(img => {
+            img.src = img.dataset.src;
+        })
+    } else {
+        // Not supported!
+
+        console.log('boo!')
+
+        const script = document.createElement('script')
+        script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js';
+        document.body.appendChild(script);
+    }
               
 
 
